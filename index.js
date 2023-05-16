e2cee6d7f8df2558d4a09d716fa7586491eaf196
SomaanIqbal <isomaan@gmail.com>
2023-05-16 14:29:14 -0700

    about(topics)

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-function isRelevant (topics, paragraph) {
-//search for topic keyword by iterating through paragraph
-let lower = paragraph.toLowerCase();
-   for (let i = 0; i < topics.length; i++) {
-    if (lower.includes(topics[i].toLowerCase())) {
-       return true;
+function isRelevant(topics, paragraph) {
+    //search for topic keyword by iterating through paragraph
+    let lower = paragraph.toLowerCase();
+    for (let i = 0; i < topics.length; i++) {
+        if (lower.includes(topics[i].toLowerCase())) {
+            return true;
         }
     }
     return false;
 }
-    console.log(isRelevant(['cat','Fox'], "The quick brown fox jumps over the lazy dog")) // true
+// console.log(isRelevant(['cat', 'Fox'], "The quick brown fox jumps over the lazy dog")) // true
+// --------------------------------------------------
 
 
-about (topics) {
+// return a function that takes a paragraph
 
-}
+// returns paragrpaph and checks if relevent to topics
 
+
+
+const about = (topics) => (paragraph) => isRelevant(topics, paragraph)
+
+// const about = (topics) => {
+//     const aboutCats = (paragraph) => {
+//         return isRelevant = () => {
+//             return true;
+//         }
+//     }
+// }
+
+
+
+
+const aboutCats = about(["cat", "kitten", "cats"]);
+// console.log(aboutCats("The quick brown fox jumps over the lazy dog."));
+console.log(aboutCats("The quick brown cat jumps over the lazy dog.")); // true
