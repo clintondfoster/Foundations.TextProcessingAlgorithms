1f0d14e77ffa2337fac619e4724d3364d47c1bdc
clinton foster <clintondavidfoster@gmail.com>
2023-05-16 18:00:54 -0400

    Function getRelevantSentences

--- index.js
+++ index.js
@@ -9,28 +9,21 @@ function isRelevant(topics, paragraph) {
     return false;
 }
 // console.log(isRelevant(['cat', 'Fox'], "The quick brown fox jumps over the lazy dog")) // true
-// --------------------------------------------------
-
-
-// return a function that takes a paragraph
-
-// returns paragrpaph and checks if relevent to topics
-
-
 
 const about = (topics) => (paragraph) => isRelevant(topics, paragraph)
 
-// const about = (topics) => {
-//     const aboutCats = (paragraph) => {
-//         return isRelevant = () => {
-//             return true;
-//         }
-//     }
-// }
-
-
-
-
 const aboutCats = about(["cat", "kitten", "cats"]);
-// console.log(aboutCats("The quick brown fox jumps over the lazy dog."));
-console.log(aboutCats("The quick brown cat jumps over the lazy dog.")); // true
+//console.log(aboutCats("The quick brown fox jumps over the lazy dog.")); //false
+//console.log(aboutCats("The quick brown cat jumps over the lazy dog.")); //true
+
+
+const getRelevantSentences = (criteriaFn, sentences) => (paragraph) => criteriaFn(isRelevant(topics, paragraph))
+
+const sentences = [
+    "I have a pet cat.",
+    "I have a pet dog.",
+    "I don't have a pet.",
+  ];
+
+  console.log(getRelevantSentences(aboutCats, sentences)) //["I have a pet cat."]
+//   console.log(getRelevantSentences((s) => s.length > 17, sentences)) //["I don't have a pet."]
